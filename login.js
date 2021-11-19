064cffcb516079cfa02fb4b89047ef58a9aea17a
Jorge Freitas <80436467+jorgelcff@users.noreply.github.com>
2021-11-18 23:50:19 -0300

    Criação da tela de Login
    Início da marcação da tela feito.

--- login.js
+++ login.js
@@ -1,13 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import Artigo from "./src/components/artigo/index";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Matheus pior do mundo</Text>
-      <Artigo/>
+      <Text style={styles.titulo}>
+        Escuta Ativa
+      </Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder ="Email"
+      />
+
+      <TextInput
+        style={styles.input}
+        secureTextEntry={true}
+        placeholder ="Senha"
+      />
+
+      <TouchableOpacity
+        style={styles.botao}
+        onPress={ () =>{this.clicou()}}
+      > 
+
+        <Text style={styles.botaoTexto}>
+          Entrar
+        </Text>
+
+      </TouchableOpacity>
+
+      <Text style={styles.texto}>
+      Ainda não possui uma conta?
+      </Text>
+
+      <Text style={styles.texto}
+      onPress={() => Linking.openURL('')}>
+      Clique Aqui
+      </Text>
+
+
+
+
       <StatusBar style="auto" />
     </View>
   );
